90140deea25b3e759795bf7338a10a7170779b8b
RPE8 <vitali.shuman96@gmail.com>
2023-04-04 17:51:14 +0300

    useRouter() Hook

--- sandbox/src/app/(shop)/shops/[[...shop]]/page.tsx
+++ sandbox/src/app/(shop)/shops/[[...shop]]/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Page({
   params: { shop },
 }: {
   params: { shop: string[] };
 }) {
   console.log(shop);
-  return <div>My Shop: {shop}</div>;
+  const router = useRouter();
+  return (
+    <div>
+      My Shop: {shop}
+      <br />{" "}
+      <button type="button" onClick={() => router.push(`/items/1/2/3`)}>
+        To items
+      </button>
+    </div>
+  );
 }
