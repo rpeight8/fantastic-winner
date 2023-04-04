ed588d896f908ec66331cf75ae499090f91821e7
RPE8 <vitali.shuman96@gmail.com>
2023-04-04 19:32:29 +0300

    Error Handling

--- sandbox/src/app/(shop)/sub1/[sub2]/sub3/[...sub4]/page.tsx
+++ sandbox/src/app/(shop)/sub1/[sub2]/sub3/[...sub4]/page.tsx
@@ -4,6 +4,7 @@ export default function Page({
   params: { sub2: string; sub4: string[] };
 }) {
   console.log(params);
+  if (params.sub2 === "heheSUB2") throw new Error("Error");
   return (
     <div>
       My sub2: {params.sub2} <br /> My sub4: {params.sub4}
