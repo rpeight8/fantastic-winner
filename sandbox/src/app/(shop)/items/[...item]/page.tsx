05d1e3e8454f56e6699eb08859a44f4d6b3401e1
RPE8 <vitali.shuman96@gmail.com>
2023-04-04 17:47:04 +0300

    Linking to Dynamic Segments

--- sandbox/src/app/(shop)/items/[...item]/page.tsx
+++ sandbox/src/app/(shop)/items/[...item]/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 export default function Page({
   params: { item },
 }: {
   params: { item: string };
 }) {
   console.log(item);
-  return <div>My Post: {item}</div>;
+  const sub2 = "heheSUB2";
+  return (
+    <div>
+      My Post: {item} <br />{" "}
+      <Link href={`/sub1/${sub2}/sub3/${item}`}>To SUB</Link>
+    </div>
+  );
 }
