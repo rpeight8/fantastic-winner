017d2b142f96c70bd6703e4dc27f9921d5cfcd39
RPE8 <vitali.shuman96@gmail.com>
2023-04-04 16:53:03 +0300

    Typo

--- sandbox/src/app/(shop)/sub1/[sub2]/sub3/[...sub4]/page.tsx
+++ sandbox/src/app/(shop)/sub1/[sub2]/sub3/[...sub4]/page.tsx
@@ -1,12 +1,12 @@
 export default function Page({
   params,
 }: {
-  params: { sub3: string; sub4: string[] };
+  params: { sub2: string; sub4: string[] };
 }) {
   console.log(params);
   return (
     <div>
-      My sub3: {params.sub2} <br /> My sub4: {params.sub4}
+      My sub2: {params.sub2} <br /> My sub4: {params.sub4}
     </div>
   );
 }
